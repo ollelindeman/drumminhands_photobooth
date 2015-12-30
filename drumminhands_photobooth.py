4487bc726be4688b8ea44639e3eb2d8840f808fa
Olle Lindeman <olle.lindeman@gmail.com>
2015-12-30 11:02:24 +0000

    First working code

--- drumminhands_photobooth.py
+++ drumminhands_photobooth.py
@@ -12,7 +12,6 @@ import picamera # http://picamera.readthedocs.org/en/release-1.4/install2.html
 import atexit
 import sys
 import socket
-import pygame
 import pytumblr # https://github.com/tumblr/pytumblr
 import config
 from signal import alarm, signal, SIGALRM, SIGKILL
@@ -28,21 +27,12 @@ button1_pin = 22 # pin for the big red button
 button2_pin = 18 # pin for button to shutdown the pi
 button3_pin = 16 # pin for button to end the program, but not shutdown the pi
 
-post_online = 1 # default 1. Change to 0 if you don't want to upload pics.
-total_pics = 4 # number of pics to be taken
-capture_delay = 2 # delay between pics
+post_online = 0 # default 1. Change to 0 if you don't want to upload pics.
+total_pics = 8 # number of pics to be taken
+capture_delay = 0.5 # delay between pics
 prep_delay = 3 # number of seconds at step 1 as users prep to have photo taken
-gif_delay = 100 # How much time between frames in the animated gif
+gif_delay = 50 # How much time between frames in the animated gif
 restart_delay = 5 # how long to display finished message before beginning a new session
-
-monitor_w = 800
-monitor_h = 480
-transform_x = 640 # how wide to scale the jpg when replaying
-transfrom_y = 480 # how high to scale the jpg when replaying
-offset_x = 80 # how far off to left corner to display photos
-offset_y = 0 # how far off to left corner to display photos
-replay_delay = 1 # how much to wait in-between showing pics on-screen after taking
-replay_cycles = 2 # how many times to show each photo on-screen after taking
 
 test_server = 'www.google.com'
 real_path = os.path.dirname(os.path.realpath(__file__))
@@ -69,7 +59,7 @@ GPIO.setup(button3_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP) # falling edge detect
 GPIO.output(led1_pin,False);
 GPIO.output(led2_pin,False);
 GPIO.output(led3_pin,False);
-GPIO.output(led4_pin,False); #for some reason the pin turns on at the beginning of the program. why?????????????????????????????????
+GPIO.output(led4_pin,False); #for some reason the pin turns on at the beginning of the program.
 
 #################
 ### Functions ###
@@ -126,61 +116,21 @@ def is_connected():
      pass
   return False    
 
-def init_pygame():
-    pygame.init()
-    size = (pygame.display.Info().current_w, pygame.display.Info().current_h)
-    pygame.display.set_caption('Photo Booth Pics')
-    pygame.mouse.set_visible(False) #hide the mouse cursor	
-    return pygame.display.set_mode(size, pygame.FULLSCREEN)
-
-def show_image(image_path):
-    screen = init_pygame()
-    img=pygame.image.load(image_path) 
-    img = pygame.transform.scale(img,(transform_x,transfrom_y))
-    screen.blit(img,(offset_x,offset_y))
-    pygame.display.flip()
-
-def display_pics(jpg_group):
-    # this section is an unbelievable nasty hack - for some reason Pygame
-    # needs a keyboardinterrupt to initialise in some limited circs (second time running)
-
-    class Alarm(Exception):
-        pass
-    def alarm_handler(signum, frame):
-        raise Alarm
-    signal(SIGALRM, alarm_handler)
-    alarm(3)
-    try:
-        screen = init_pygame()
-
-        alarm(0)
-    except Alarm:
-        raise KeyboardInterrupt
-    for i in range(0, replay_cycles): #show pics a few times
-		for i in range(1, total_pics+1): #show each pic
-			filename = config.file_path + jpg_group + "-0" + str(i) + ".jpg"
-                        show_image(filename);
-			time.sleep(replay_delay) # pause 
-				
 # define the photo taking function for when the big button is pressed 
 def start_photobooth(): 
 	################################# Begin Step 1 ################################# 
-	show_image(real_path + "/blank.png")
 	print "Get Ready"
 	GPIO.output(led1_pin,True);
-	show_image(real_path + "/instructions.png")
 	sleep(prep_delay) 
 	GPIO.output(led1_pin,False)
 
-	show_image(real_path + "/blank.png")
-	
 	camera = picamera.PiCamera()
 	pixel_width = 500 #use a smaller size to process faster, and tumblr will only take up to 500 pixels wide for animated gifs
-	pixel_height = monitor_h * pixel_width // monitor_w
+	pixel_height = 500
 	camera.resolution = (pixel_width, pixel_height) 
 	camera.vflip = True
 	camera.hflip = False
-	camera.saturation = -100 # comment out this line if you want color images
+	#camera.saturation = -100 # comment out this line if you want color images
 	camera.start_preview()
 	
 	sleep(2) #warm up camera
@@ -202,10 +152,6 @@ def start_photobooth():
 		camera.close()
 	########################### Begin Step 3 #################################
 	print "Creating an animated gif" 
-	if post_online:
-		show_image(real_path + "/uploading.png")
-	else:
-		show_image(real_path + "/processing.png")
 
 	GPIO.output(led3_pin,True) #turn on the LED
 	graphicsmagick = "gm convert -delay " + str(gif_delay) + " " + config.file_path + now + "*.jpg " + config.file_path + now + ".gif" 
@@ -231,22 +177,10 @@ def start_photobooth():
 	
 	########################### Begin Step 4 #################################
 	GPIO.output(led4_pin,True) #turn on the LED
-	try:
-		display_pics(now)
-	except Exception, e:
-		tb = sys.exc_info()[2]
-		traceback.print_exception(e.__class__, e, tb)
-	pygame.quit()
 	print "Done"
 	GPIO.output(led4_pin,False) #turn off the LED
 	
-	if post_online:
-		show_image(real_path + "/finished.png")
-	else:
-		show_image(real_path + "/finished2.png")
-	
 	time.sleep(restart_delay)
-	show_image(real_path + "/intro.png");
 
 ####################
 ### Main Program ###
@@ -254,10 +188,10 @@ def start_photobooth():
 
 # when a falling edge is detected on button2_pin and button3_pin, regardless of whatever   
 # else is happening in the program, their function will be run   
-GPIO.add_event_detect(button2_pin, GPIO.FALLING, callback=shut_it_down, bouncetime=300) 
+#GPIO.add_event_detect(button2_pin, GPIO.FALLING, callback=shut_it_down, bouncetime=300) 
 
 #choose one of the two following lines to be un-commented
-GPIO.add_event_detect(button3_pin, GPIO.FALLING, callback=exit_photobooth, bouncetime=300) #use third button to exit python. Good while developing
+#GPIO.add_event_detect(button3_pin, GPIO.FALLING, callback=exit_photobooth, bouncetime=300) #use third button to exit python. Good while developing
 #GPIO.add_event_detect(button3_pin, GPIO.FALLING, callback=clear_pics, bouncetime=300) #use the third button to clear pics stored on the SD card from previous events
 
 # delete files in folder on startup
@@ -275,8 +209,6 @@ GPIO.output(led1_pin,False); #turn off the lights
 GPIO.output(led2_pin,False);
 GPIO.output(led3_pin,False);
 GPIO.output(led4_pin,False);
-
-show_image(real_path + "/intro.png");
 
 while True:
         GPIO.wait_for_edge(button1_pin, GPIO.FALLING)
